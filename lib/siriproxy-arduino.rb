d148198eec01b4f9f9e9884f07e584043075e929
Nico Ritschel <ritschel@gmail.com>
2013-01-30 17:59:52 -0800

    Added examples for RGB led

--- lib/siriproxy-arduino.rb
+++ lib/siriproxy-arduino.rb
@@ -16,6 +16,7 @@ class SiriProxy::Plugin::Arduino < SiriProxy::Plugin
     #if you have custom configuration options, process them here!
     board = Dino::Board.new(Dino::TxRx.new)
     @led = Dino::Components::Led.new(pin:13, board: board)
+    @rgb_led = Dino::Components::RgbLed.new(pins: {red: 12, green: 11, blue: 10}, board: board)
   end
 
   listen_for /light(s)? on/i do
@@ -29,4 +30,17 @@ class SiriProxy::Plugin::Arduino < SiriProxy::Plugin
     say "Light off" 
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
+
+  listen_for /light(s)? green/i do
+    @rgb_led.green
+  end
+
+  listen_for /light(s)? blue/i do
+    @rgb_led.blue
+  end
+
+  listen_for /light(s)? red/i do
+    @rgb_led.red
+  end
+
 end
